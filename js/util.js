b35dc79d94ac9ae2d8f1adaa64ceca2bb96c0189
Remcore <yuzefatov@gmail.com>
2018-05-21 17:15:10 +0700

    update slider

--- js/util.js
+++ js/util.js
@@ -146,19 +146,38 @@ $('.slider').slick({
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        arrows: false
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 425,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 375,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
       }
     }
+
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
